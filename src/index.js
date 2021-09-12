602a3dc406ecf274035a7e1abac2aa02c0dd0bad
Aniket <rajeshwarkumar9204@gmail.com>
2021-09-12 23:51:10 +0530

    create basics of index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 const express = require("express");
 const app = express();
+
+// Setting up the port
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.render("Hello, world!!!");
+});
+
+app.listen(port, () => {
+  console.log(`Server up and runnin at http://localhost:${PORT}`);
+});
